2ba11d7cb6e58aa37b47c5888343c583e23b0b31
Charuvarthan-T <charuvarthan05@gmail.com>
2025-08-21 21:03:03 +0530

    token.rs - to define the vocabulary of language

--- mini-c/src/token.rs
+++ mini-c/src/token.rs
@@ -1,2 +1,18 @@
-// token.rs
-// Token definitions for mini-c
+// to define the vocabulary of language
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    Int,
+    Return,
+    Ident(String),
+    Number(i64),
+    String(String),
+    Semicolon,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Assign,
+    Comma,
+    EOF,
+}
