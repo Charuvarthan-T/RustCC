41a5513763d720434c14671ea01bc234560b874b
Charuvarthan-T <charuvarthan05@gmail.com>
2025-10-08 20:05:13 +0530

    final lower level conversion before assembly

--- mini-c/src/lower.rs
+++ mini-c/src/lower.rs
@@ -1,15 +1,20 @@
 use crate::ast::*;
 use crate::ir::{FunctionIR, Instr, Operand};
 
+// Lower AST to IR
+// the below LowerState struct helps generate unique temporary names
 struct LowerState {
     tmp: usize,
 }
 
+// Lower an expression to IR, appending instructions to `instrs` and returning an Operand
 impl LowerState {
     fn new() -> Self { LowerState { tmp: 0 } }
     fn gen_tmp(&mut self) -> String { let id = self.tmp; self.tmp += 1; format!("t{}", id) }
 }
 
+
+// Lower an expression recursively
 fn lower_expr(expr: &Expr, state: &mut LowerState, instrs: &mut Vec<Instr>) -> Operand {
     match expr {
         Expr::Number(n) => Operand::ConstInt(*n),
@@ -17,18 +22,27 @@ fn lower_expr(expr: &Expr, state: &mut LowerState, instrs: &mut Vec<Instr>) -> O
         Expr::CharLiteral(c) => Operand::ConstInt(*c as i64),
         Expr::Ident(name) => Operand::Local(name.clone()),
         Expr::Unary { op, expr } => {
+
+            // lower sub-expression
             let o = lower_expr(expr, state, instrs);
             let dest = state.gen_tmp();
             match op {
+
+                // Neg and Not are implemented as binary ops with 0 as right operand
                 crate::ast::UnaryOp::Neg => {
                     instrs.push(Instr::BinOp { dest: dest.clone(), op: "neg".to_string(), left: o.clone(), right: Operand::ConstInt(0) });
                 }
+
+                // Not is implemented as binary ops with 0 as right operand
                 crate::ast::UnaryOp::Not => {
                     instrs.push(Instr::BinOp { dest: dest.clone(), op: "not".to_string(), left: o.clone(), right: Operand::ConstInt(0) });
                 }
             }
             Operand::Temp(dest)
         }
+
+
+        // Lower a binary expression
         Expr::Binary { op, left, right } => {
             let l = lower_expr(left, state, instrs);
             let r = lower_expr(right, state, instrs);
@@ -39,14 +53,22 @@ fn lower_expr(expr: &Expr, state: &mut LowerState, instrs: &mut Vec<Instr>) -> O
                 crate::ast::BinaryOp::Mul => "*",
                 crate::ast::BinaryOp::Div => "/",
             };
+
+            // emit binary operation instruction
             instrs.push(Instr::BinOp { dest: dest.clone(), op: opname.to_string(), left: l, right: r });
             Operand::Temp(dest)
         }
+
+
+        // Assignment: evaluate right-hand side, store in local variable
         Expr::Assign { name, value } => {
             let v = lower_expr(value, state, instrs);
             instrs.push(Instr::StoreLocal { name: name.clone(), src: v.clone() });
             Operand::Local(name.clone())
         }
+
+
+        // Function call: evaluate args, emit call instruction
         Expr::Call { name, args } => {
             let mut op_args = Vec::new();
             for a in args {
@@ -56,10 +78,17 @@ fn lower_expr(expr: &Expr, state: &mut LowerState, instrs: &mut Vec<Instr>) -> O
             instrs.push(Instr::Call { dest: Some(dest.clone()), name: name.clone(), args: op_args });
             Operand::Temp(dest)
         }
+
+        // String literals are not directly representable as operands; handled in codegen
         Expr::StringLiteral(s) => Operand::ConstString(s.clone()),
     }
 }
 
+
+
+
+
+// Lower a whole program
 pub fn lower_program(prog: &crate::ast::Program) -> Vec<FunctionIR> {
     let mut res = Vec::new();
     for func in &prog.functions {
@@ -81,6 +110,8 @@ pub fn lower_program(prog: &crate::ast::Program) -> Vec<FunctionIR> {
                 }
             }
         }
+
+        // create FunctionIR
         let fir = FunctionIR { name: func.name.clone(), params: func.params.iter().map(|(_, n)| n.clone()).collect(), instrs };
         res.push(fir);
     }
